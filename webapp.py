4903c6d18dd276b538426a3642e18c9d2533a6b8
Juan F. Codagnone <juan@zaubersoftware.com>
2008-09-05 23:53:05 +0000

    pagina del geoinfo

--- webapp.py
+++ webapp.py
@@ -20,10 +20,17 @@
 
 from sys import path
 path.append('site-packages')
-import web, os
+import web, os, urllib2
+from web import template
+from BeautifulSoup import BeautifulStoneSoup
+from datetime import datetime
+
+
 
 urls = (
     '/',                                    'RootController',
+    '/place/(\d+)',                         'PlaceController',
+    '/place/(\d+)/',                         'PlaceController',
 )
 
 
@@ -32,9 +39,93 @@ class RootController:
         print render.header()
         print render.root()
         print render.footer()
+
+class PlaceController:
+    def GET(self, id):
+        print render.header()
+        print render.place(flof.geoinfo(id), 'hola')
+        print render.footer()
+
+class Flof:
+    headers = {}
+    URL_THUMB = 'http://test.flof.com.ar/bin/spot/image/?action=thumb&imageid=%s'
+    URL_SPOT = 'http://localhost:9091/feeds/xml/geoinfo/%s/'
+
+    def geoinfo(self, id):
+        opener = urllib2.build_opener()
+        req = urllib2.Request(self.URL_SPOT % id,{}, self.headers)
+        y = opener.open(req)
+        xml = y.read()
+        y.close()
+        soup = BeautifulStoneSoup(xml,  convertEntities='xml', smartQuotesTo='xml')
+        ret = { 'urls': [],
+                'geocoding': [],
+                'photos': [],
+                'reviews': [],
+                'name':  soup.geoinfo['name'],
+                'lat':  soup.geoinfo['lat'],
+                'lon':  soup.geoinfo['lon'],
+              }
+        for i in soup.findChildren('spot'):
+            if i['geocoding']:
+                ret['geocoding'].append(i['geocoding'])
+            if i['url']:
+                ret['urls'].append(i['url'])
+            if i['photo_url']:
+                #ret['photos'].append(i['photo_url'])
+                ret['photos'].append({
+                       'url':   i['photo_url'],
+                       'thumb': self.URL_THUMB % i['id'],
+                   })
+
+            if i['description']:
+                ret['reviews'].append({
+                    'owner': i['owner'],
+                    'text':  i['description'],
+                    'date':  datetime.strptime(i.date.contents[0][:-9], 
+                                              "%Y-%m-%d %H:%M:%S")
+                })
+        ret['urls'].sort()
+        ret['geocoding'].sort()
+        ret['photos'].sort()
+        ret['reviews'].sort(lambda x, y: cmp(y['date'], x['date']))
+
+        ret['urls'] = uniquer(ret['urls'])
+        ret['geocoding'] = uniquer(ret['geocoding'])
+        ret['photos'] = uniquer(ret['photos'], lambda x: x['url'])
+        labels = {}
+        for i in soup.findChildren('label'):
+            label = i['name']
+            if label in labels:
+                labels[label] = labels[label] + 1
+            else:
+                labels[label] = 1
+        ret['labels'] = labels
+
+        return ret 
+
+def uniquer(seq, idfun=None):
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        # in old Python versions:
+        # if seen.has_key(marker)
+        # but in new ones:
+        if marker in seen: continue
+        seen[marker] = 1
+        result.append(item)
+    return result
+
+    
 #########################################################################
 
 render = web.template.render('templates/', cache='DEV' not in os.environ)
+template.Template.globals['len'] = len
+flof = Flof()
+print flof.geoinfo('10009')
 if __name__ == "__main__":
 
     if 'DEV' in os.environ:
