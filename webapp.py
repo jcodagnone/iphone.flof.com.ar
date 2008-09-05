d85e84cd51d9825ceeb1461b8f901325e61a5365
Juan F. Codagnone <juan@zaubersoftware.com>
2008-09-05 14:25:42 +0000

    esqueleto para flof for iphone :D

---
+++ webapp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# iphone.flof.com.ar provides a nice UI to access flof data from an iphone
+#
+# Copyright (c) 2008 by Zauber S.A. <www.zauber.com.ar>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from sys import path
+path.append('site-packages')
+import web, os
+
+urls = (
+    '/',                                    'RootController',
+)
+
+class RootController:
+    def GET(self):
+        print 'hello world1'
+
+#########################################################################
+
+render = web.template.render('templates/', cache='DEV' not in os.environ)
+if 'DEV' in os.environ:
+    middleware = [web.reloader]
+else:
+    middleware = []
+
+if __name__ == "__main__":
+    web.run(urls, globals(), *middleware)
